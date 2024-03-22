dc5e84cb1a0760caed1cd81908110026a219ced7
Jeff Katzy <jeffreyerickatz@gmail.com>
2024-03-22 19:48:50 -0400

    update reading

--- llm-scraper/api/app/server.py
+++ llm-scraper/api/app/server.py
@@ -12,16 +12,6 @@ def init_db_command():
     """Create database tables and seed data."""
     db.create_all()
 
-@app.cli.command("seed-db")
-def seed_db():
-    scraping_count = db.session.query(Scraping).count()
-    position_count = db.session.query(Position).count()
-    print(f"Will seed if scrapings and positions is over zero, currently there are \n {scraping_count} scrapings and \n {position_count} positions ")
-    if not scraping_count:
-        seed_scrapings_from_csv('./data/scrapings.csv')
-    if not position_count:
-        seed_positions_from_csv('./data/positions.csv')
-
 @app.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Position': Position, 'Scraping': Scraping}
